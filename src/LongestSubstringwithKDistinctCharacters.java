3f679e13d3f1d4b5deada6f564664cc2a69d4ed8
PavanGoudPujari <pavanpujari5656@gmail.com>
2026-03-07 17:44:34 -0600

    Length Of Longest SubString K Distinct

---
+++ src/LongestSubstringwithKDistinctCharacters.java
@@ -0,0 +1,44 @@
+// Problem:
+// Longest substring with at most K distinct characters
+//
+// Example:
+// String = "araaci"
+// K = 2
+//
+// Answer:
+// "araa" -> length 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class LongestSubstringwithKDistinctCharacters {
+}
